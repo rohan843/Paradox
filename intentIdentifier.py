8b8085ab4c15545d47f22e69352bd6b5f0acc197
rohan843 <rohan.sharma2934@gmail.com>
2023-02-13 16:43:02 +0530

    create IntentIdentifier class

--- intentIdentifier.py
+++ intentIdentifier.py
@@ -1,6 +1,55 @@
-class IntentIdentifier():
-    def __init__(self):
-        pass
+import time
 
-    def parseUserRequest(self, userRequest):
-        pass
+def runAction(s: str, actions: list):
+    i = intentIdentifier.getMatchingIntentIndex(s)
+    if i == -1:
+        print('Not a supported intent')
+    else:
+        actions[i][0](actions[i][1])
+
+class IntentIdentifier:
+    def __init__(self, intents: list, thresh: float = 0.5):
+        self.__intents = intents
+        self.__thresh = thresh
+
+    def getMatchingIntentIndex(self, userRequest: str):
+        mxProb = 0
+        mxI = -1
+        for i in range(len(self.__intents)):
+            matchingProb = self.__getMatchingProb(
+                userRequest, self.__intents[i])
+            if matchingProb > mxProb:
+                mxI = i
+                mxProb = matchingProb
+        return mxI if mxProb >= self.__thresh else -1
+
+    def __getMatchingProb(self, s1: str, s2: str):
+        # TODO: Return the probability that s1 and s2 mean the same
+        return 0 if s1 != s2 else 1
+
+
+def turnLights(params: list):
+    on = params[0]
+    if on:
+        print('Turning lights ON')
+    else:
+        print('Turning lights OFF')
+
+def getTime(params: list):
+    print('The time is', time.ctime())
+
+if __name__ == '__main__':
+    intents = [
+        'Turn on the lights',
+        'Turn off the lights',
+        'What is the time?'
+    ]
+    actions = [
+        [turnLights, [True]],
+        [turnLights, [False]],
+        [getTime, []]
+    ]
+    intentIdentifier = IntentIdentifier(intents)
+    for s in intents:
+        runAction(s, actions)
+    runAction('Start the timer.', actions)
