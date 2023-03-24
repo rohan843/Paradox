ee24f4fe4c2f716d54caa80072836f4f20ece6fa
rohan843 <rohan.sharma2934@gmail.com>
2023-03-24 21:03:00 +0530

    finish siamese net integration

--- intentIdentifier.py
+++ intentIdentifier.py
@@ -1,10 +1,69 @@
 import time
+import os
+import numpy as np
+import tensorflow as tf
 
 
 class IntentIdentifier:
     def __init__(self, intents: list, thresh: float = 0.5):
         self.__intents = intents
         self.__thresh = thresh
+
+        path_to_glove_file = os.path.join(
+            'glove.6B.100d.txt'
+        )
+        embeddings_index = {}
+        with open(path_to_glove_file) as f:
+            for line in f:
+                word, coefs = line.split(maxsplit=1)
+                coefs = np.fromstring(coefs, "f", sep=" ")
+                embeddings_index[word] = coefs
+        print("Found %s word vectors." % len(embeddings_index))
+        self.vectorizer = tf.keras.layers.TextVectorization(
+            output_sequence_length=20)
+        self.vectorizer.set_vocabulary(list(embeddings_index.keys()))
+        print(
+            f'Stored {len(self.vectorizer.get_vocabulary())} words in vocabulary.')
+        voc = self.vectorizer.get_vocabulary()
+        word_index = dict(zip(voc, range(len(voc))))
+        num_tokens = len(voc) + 2
+        embedding_dim = 100  # We are using 100 dim embeddings
+        hits = 0
+        misses = 0
+
+        # Prepare embedding matrix
+        embedding_matrix = np.zeros((num_tokens, embedding_dim))
+        for word, i in word_index.items():
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                # Words not found in embedding index will be all-zeros.
+                # This includes the representation for "padding" and "OOV"
+                embedding_matrix[i] = embedding_vector
+                hits += 1
+            else:
+                misses += 1
+        print("Converted %d words (%d misses)" % (hits, misses))
+        self.embedding_layer = tf.keras.layers.Embedding(
+            num_tokens,
+            embedding_dim,
+            embeddings_initializer=tf.keras.initializers.Constant(
+                embedding_matrix
+            ),
+            trainable=False,
+            input_length=20
+        )
+
+        self.model_path = 'sentence_matching_model'
+        self.model = tf.keras.models.load_model(self.model_path)
+
+    def getSimilarityProbability(self, s1: str, s2: str):
+        s1 = s1.lower()
+        s2 = s2.lower()
+        v1 = self.vectorizer(s1)
+        v2 = self.vectorizer(s2)
+        e1 = self.embedding_layer(v1)
+        e2 = self.embedding_layer(v2)
+        return self.model.predict({"sentence1": np.array(e1).reshape((1, 20, 100)), "sentence2": np.array(e2).reshape((1, 20, 100))}, verbose=0)[0][0]
 
     def getMatchingIntentIndex(self, userRequest: str):
         mxProb = 0
@@ -18,8 +77,8 @@ class IntentIdentifier:
         return mxI if mxProb >= self.__thresh else -1
 
     def __getMatchingProb(self, s1: str, s2: str):
-        # TODO: Return the probability that s1 and s2 mean the same
-        return 0 if s1 != s2 else 1
+        return self.getSimilarityProbability(s1, s2)
+        # return 0 if s1 != s2 else 1
 
 
 def turnLights(params: list):
@@ -34,10 +93,10 @@ def getTime(params: list):
     print('The time is', time.ctime())
 
 
-def runAction(s: str, actions: list):
+def runAction(s: str, actions: list, defaultAction):
     i = intentIdentifier.getMatchingIntentIndex(s)
     if i == -1:
-        print('Not a supported intent')
+        defaultAction()
     else:
         actions[i][0](actions[i][1])
 
@@ -53,7 +112,10 @@ if __name__ == '__main__':
         [turnLights, [False]],
         [getTime, []]
     ]
+
+    def defaultAction():
+        print('Not a supported intent')
     intentIdentifier = IntentIdentifier(intents)
     for s in intents:
-        runAction(s, actions)
-    runAction('Start the timer.', actions)
+        runAction(s, actions, defaultAction)
+    runAction('Start the timer.', actions, defaultAction)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from triggerword import TriggerWordListener
 from speechRec import SpeechToText
+from intentIdentifier import IntentIdentifier
 
 SPEECH_LISTENING_DURATION = 5
 TRIGGER_WORD_PROB_THRESH = 0.97
@@ -7,9 +8,12 @@ TRIGGER_WORD_PROB_THRESH = 0.97
 
 def action(params: list):
     sttEngg = params[0]
-    print(sttEngg.captureUtterance())
+    s = sttEngg.captureUtterance()
+    print(s)
+    if 'close' in s or 'exit' in s:
+        exit()
 
-
+# TODO: Use IntentIdentifier here, as needed
 sttEngg = SpeechToText(SPEECH_LISTENING_DURATION)
 triggerWordListener = TriggerWordListener(
     action, [sttEngg], TRIGGER_WORD_PROB_THRESH)
